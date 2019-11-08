cd6ef793e8b4f346149965f2e0c28bc7b53c2b41
raheelansari <raheelansari7@gmail.com>
2019-11-08 19:28:42 +0500

    ADM SERVICES

--- src/Handler/Hospital/PendingAdmissionServicesHandler.java
+++ src/Handler/Hospital/PendingAdmissionServicesHandler.java
@@ -177,8 +177,8 @@ public class PendingAdmissionServicesHandler {
                 + Database.DCMS.invoiceDetail + " IVD,                  \n"
                 + Database.DCMS.definitionTypeDetail + " DEP,           \n"
                 + Database.DCMS.CPT + " CPT                             \n"
-                + " WHERE PAH.IVM.ADMISSION_NO = '" + admissionNo + "'  \n"
-                + "   AND PAH.IVM.DEPARTMENT_ID = " + departmentId + "  \n"
+                + " WHERE IVM.ADMISSION_NO = '" + admissionNo + "'  \n"
+                + "   AND IVD.DEPARTMENT_ID = " + departmentId + "  \n"
                 + "   AND IVM.INVOICE_NO = IVD.INVOICE_NO               \n"
                 + "   AND IVD.DEPARTMENT_ID = DEP.ID                    \n"
                 + "   AND IVD.CPT_ID = CPT.CPT_ID                       \n";
