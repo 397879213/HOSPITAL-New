20019ce62c9c5e8c18c5223618ae7b1d7202cd39
raheelansari <raheelansari7@gmail.com>
2019-11-12 17:52:22 +0500

    Order

--- src/Handler/LTU/HandlerTest.java
+++ src/Handler/LTU/HandlerTest.java
@@ -6,6 +6,7 @@
 package Handler.LTU;
 
 import BO.LTU.DialysisBO;
+import Process.Order;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -108,16 +109,16 @@ public class HandlerTest {
         return Constants.dao.insertData(lstInr, columns);
     }
 
-    public String selectMaster(String admNo) {
+    public List<Order> selectMaster(String admNo) {
 
         String[] selectColumns = {"-", "ORDER_TYPE_ID", "ORDER_NO", "LOCATION_ID",
-            "LOC_DESCRIPTION", "COMPLETE_ORDER_NO", "PATIENT_ID", "ORDER_DATE", 
-            "INHOUSE_DOCTOR_ID", "INHOUSE_DOCTOR_NAME", "REFERRING_DOCTOR_ID",  
+            "LOC_DESCRIPTION", "COMPLETE_ORDER_NO", "PATIENT_ID", "ORDER_DATE",
+            "INHOUSE_DOCTOR_ID", "INHOUSE_DOCTOR_NAME", "REFERRING_DOCTOR_ID",
             "TRN_DATE", "ORDER_BY", "ORDER_PERSON", "TERMINAL_ID", "MOBILITY_STATUS_ID",
-            "ORDER_LOCATION_ID", "ORDER_LOCATION_DESC", "USER_LOCATION_ID", 
-            "USER_LOCATION_DESC", "CUSTOMER_ID", "ADMISSION_NO", "CLIENT_ID", 
-            "CLIENT_NAME","MANUAL_PATIENT_ID", "ORG_ID", "PRIORITY"};
- //"REFERRING_DOCTOR_NAME",
+            "ORDER_LOCATION_ID", "ORDER_LOCATION_DESC", "USER_LOCATION_ID",
+            "USER_LOCATION_DESC", "CUSTOMER_ID", "ADMISSION_NO", "CLIENT_ID",
+            "CLIENT_NAME", "MANUAL_PATIENT_ID", "ORG_ID", "PRIORITY"};
+        //"REFERRING_DOCTOR_NAME",
         String query
                 = "SELECT OM.ORDER_TYPE_ID,                             \n"
                 + "       OM.ORDER_NO,                                  \n"
@@ -166,17 +167,136 @@ public class HandlerTest {
         System.out.println(query);
         List selectInvoice = Constants.dao.selectDatainList(query, selectColumns);
 
-//        List<PatientHospitalVisit> list = new ArrayList();
-//        for (int i = 0; i < selectInvoice.size(); i++) {
-//            HashMap map = (HashMap) selectInvoice.get(i);
-//            PatientHospitalVisit setCompound = new PatientHospitalVisit();
-//
-//            setCompound.setAdmissionNumber(admNo);
-//            setCompound.setAdmissionAmount(map.get("ADM_AMOUNT").toString());
-//            setCompound.setDepartmentId(map.get("DEPARTMENT_ID").toString());
-//            list.add(setCompound);
-//        }
-        return ""; // list
+        List<Order> list = new ArrayList();
+        for (int i = 0; i < selectInvoice.size(); i++) {
+            HashMap map = (HashMap) selectInvoice.get(i);
+            Order setCompound = new Order();
+            
+            setCompound.setOrderTypeId(map.get("ORDER_TYPE_ID").toString());
+            setCompound.setOrderNo(map.get("ORDER_NO").toString());
+            setCompound.setLocationId(map.get("LOCATION_ID").toString());
+            setCompound.setLocationDescription(map.get("LOC_DESCRIPTION").toString());
+            setCompound.setCompleteOrderNo(map.get("COMPLETE_ORDER_NO").toString());
+            setCompound.setPatientId(map.get("PATIENT_ID").toString());
+            setCompound.setOrderDate(map.get("ORDER_DATE").toString());
+            setCompound.setInHouseDoctorId(map.get("INHOUSE_DOCTOR_ID").toString());
+            setCompound.setInHouseDoctorName(map.get("INHOUSE_DOCTOR_NAME").toString());
+            setCompound.setReferringDoctorId(map.get("REFERRING_DOCTOR_ID").toString());
+//            setCompound.setReferringDoctor(map.get("REFERRING_DOCTOR_NAME").toString());
+            setCompound.setTrnDate(map.get("TRN_DATE").toString());
+            setCompound.setOrderBy(map.get("ORDER_BY").toString());
+            setCompound.setOrderPerson(map.get("ORDER_PERSON").toString());
+            setCompound.setTerminalId(map.get("TERMINAL_ID").toString());
+            setCompound.setMobilityStatusId(map.get("MOBILITY_STATUS_ID").toString());
+            setCompound.setOrderLocationId(map.get("ORDER_LOCATION_ID").toString());
+            setCompound.setOrderLocationDescription(map.get("ORDER_LOCATION_DESC").toString());
+            setCompound.setUserLocationId(map.get("USER_LOCATION_ID").toString());
+            setCompound.setUserLocationDescription(map.get("USER_LOCATION_DESC").toString());
+            setCompound.setCustomerId(map.get("CUSTOMER_ID").toString());
+            setCompound.setAdmissionNo(map.get("ADMISSION_NO").toString());
+            setCompound.setClientId(map.get("CLIENT_ID").toString());
+            setCompound.setClientDescription(map.get("CLIENT_NAME").toString());
+            setCompound.setManualPatientId(map.get("MANUAL_PATIENT_ID").toString());
+            setCompound.setOrganizationId(map.get("ORG_ID").toString());
+            setCompound.setPriority(map.get("PRIORITY").toString());
+            list.add(setCompound);
+        }
+        return list;
+    }
+
+    public List<Order> selectDetail(String admNo) {
+
+        String[] selectColumns = {"-", "COMPLETE_ORDER_NO", "ORDER_DETAIL_ID", 
+            "ORDER_STATUS_ID", "ORDER_STATUS_DESC","CPT_ID", "CPT_NAME", "URGENT",
+            "PATIENT_ID", "REPORT_DATE", "PRICE", "TRN_DATE", "USER_ID", "ORDER_PERSON", 
+            "TERMINAL_ID", "CPT_REMARKS", "DEPARTMENT_ID", "DEPARTMENT_DESC", 
+            "SECTION_ID", "SECTION_DESC", "HISTORY_REQUIRED", "CONSULTANCY_ID", 
+            "PERFORMING_PHYSICIAN_SHARE", "PERFORMING_CONTRACT_ID", "REPORT_NAME", 
+            "PERFORMING_PHYSICIAN_ID", "PERFORMING_PHYSICIAN_NAME", "QUANTITY", 
+            "INVOICED"};
+        //"REFERRING_DOCTOR_NAME",
+        String query
+                = "SELECT COMPLETE_ORDER_NO,                            \n"
+                + "       ORDER_DETAIL_ID,                              \n"
+                + "       ORDER_STATUS_ID,                              \n"
+                + "       OSI.DESCRIPTION  ORDER_STATUS_DESC,           \n"
+                + "       CPT_ID,                                       \n"
+                + "       CPT.DESCRIPTION  CPT_NAME,                    \n"
+                + "       URGENT,                                       \n"
+                + "       PATIENT_ID,                                   \n"
+                + "       REPORT_DATE,                                  \n"
+                + "       PRICE,                                        \n"
+                + "       TRN_DATE,                                     \n"
+                + "       USER_ID,                                      \n"
+                + "       ODP.NAME         ORDER_PERSON,                \n"
+                + "       TERMINAL_ID,                                  \n"
+                + "       CPT_REMARKS,                                  \n"
+                + "       DEPARTMENT_ID,                                \n"
+                + "       DEP.DESCRIPTION  DEPARTMENT_DESC,             \n"
+                + "       SECTION_ID,                                   \n"
+                + "       SEC.DESCRIPTION  SECTION_DESC,                \n"
+                + "       HISTORY_REQUIRED,                             \n"
+                + "       CONSULTANCY_ID,                               \n"
+                + "       PERFORMING_PHYSICIAN_SHARE,                   \n"
+                + "       PERFORMING_CONTRACT_ID,                       \n"
+                + "       REPORT_NAME,                                  \n"
+                + "       PERFORMING_PHYSICIAN_ID,                      \n"
+                + "       PSI.NAME PERFORMING_PHYSICIAN_NAME,           \n"
+                + "       QUANTITY,                                     \n"
+                + "       INVOICED                                      \n"
+                + "  FROM "+Database.DCMS.orderDetail+"          ODD,   \n"
+                + "       "+Database.DCMS.definitionTypeDetail+" OSI,   \n"
+                + "       "+Database.DCMS.CPT+"                  CPT,   \n"
+                + "       "+Database.DCMS.users+"                ODP,   \n"
+                + "       "+Database.DCMS.definitionTypeDetail+" DEP,   \n"
+                + "       "+Database.DCMS.section+"              SEC,   \n"
+                + "       "+Database.DCMS.users+"                PSI    \n"
+                + " WHERE ODD.ORDER_STATUS_ID = OSI.ID                  \n"
+                + "   AND ODD.CPT_ID = CPT.CPT_ID\n"
+                + "   AND ODD.USER_ID = ODP.USER_NAME\n"
+                + "   AND ODD.DEPARTMENT_ID = DEP.ID\n"
+                + "   AND ODD.SECTION_ID = SEC.SECTION_ID\n"
+                + "   AND ODD.PERFORMING_PHYSICIAN_ID = PSI.USER_NAME";
+
+        System.out.println(query);
+        List selectInvoice = Constants.dao.selectDatainList(query, selectColumns);
+
+        List<Order> list = new ArrayList();
+        for (int i = 0; i < selectInvoice.size(); i++) {
+            HashMap map = (HashMap) selectInvoice.get(i);
+            Order setCompound = new Order(); 
+
+            setCompound.setCompleteOrderNo(map.get("COMPLETE_ORDER_NO").toString());
+            setCompound.setOrderDetailId(map.get("ORDER_DETAIL_ID").toString());
+            setCompound.setOrderDetailId(map.get("ORDER_STATUS_ID").toString());
+            setCompound.setOrderDetailId(map.get("ORDER_STATUS_DESC").toString());
+            setCompound.setOrderDetailId(map.get("CPT_ID").toString());
+            setCompound.setOrderDetailId(map.get("CPT_NAME").toString());
+            setCompound.setOrderDetailId(map.get("URGENT").toString());
+            setCompound.setOrderDetailId(map.get("PATIENT_ID").toString());
+            setCompound.setOrderDetailId(map.get("REPORT_DATE").toString());
+            setCompound.setOrderDetailId(map.get("PRICE").toString());
+            setCompound.setOrderDetailId(map.get("TRN_DATE").toString());
+            setCompound.setOrderDetailId(map.get("USER_ID").toString());
+            setCompound.setOrderDetailId(map.get("ORDER_PERSON").toString());
+            setCompound.setOrderDetailId(map.get("TERMINAL_ID").toString());
+            setCompound.setOrderDetailId(map.get("CPT_REMARKS").toString());
+            setCompound.setOrderDetailId(map.get("DEPARTMENT_ID").toString());
+            setCompound.setOrderDetailId(map.get("DEPARTMENT_DESC").toString());
+            setCompound.setOrderDetailId(map.get("SECTION_ID").toString());
+            setCompound.setOrderDetailId(map.get("SECTION_DESC").toString());
+            setCompound.setOrderDetailId(map.get("CONSULTANCY_ID").toString());
+            setCompound.setOrderDetailId(map.get("HISTORY_REQUIRED").toString());
+            setCompound.setOrderDetailId(map.get("PERFORMING_PHYSICIAN_SHARE").toString());
+            setCompound.setOrderDetailId(map.get("PERFORMING_CONTRACT_ID").toString());
+            setCompound.setOrderDetailId(map.get("REPORT_NAME").toString());
+            setCompound.setOrderDetailId(map.get("PERFORMING_PHYSICIAN_ID").toString());
+            setCompound.setOrderDetailId(map.get("PERFORMING_PHYSICIAN_NAME").toString());
+            setCompound.setOrderDetailId(map.get("QUANTITY").toString());
+            setCompound.setOrderDetailId(map.get("INVOICED").toString());
+            list.add(setCompound);
+        }
+        return list;
     }
 
     public static void main(String[] args) {
---
+++ src/Process/Order.java
@@ -0,0 +1,285 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Process;
+
+/**
+ *
+ * @author raheelansari
+ */
+public class Order {
+    
+    private String patientId = "";
+    private String completeOrderNo = "";
+    private String orderTypeId = "";
+    private String orderNo = "";
+    private String orderDetailId = "";
+    private String locationId = "";
+    private String locationDescription = "";
+    private String orderStatusId = "";
+    private String orderStatusDescription = "";
+    private String orderDate = "";
+    private String inHouseDoctorId = "";
+    private String inHouseDoctorName = "";
+    private String referringDoctorId = "";
+    private String referringDoctorName = "";
+    private String trnDate = "";
+    private String orderBy = "";
+    private String orderPerson = "";
+    private String terminalId = "";
+    private String mobilityStatusId = "";
+    private String orderLocationId = "";
+    private String orderLocationDescription = "";
+    private String userLocationId = "";
+    private String userLocationDescription = "";
+    private String customerId = "";
+    private String admissionNo = "";
+    private String clientId = "";
+    private String clientDescription = "";
+    private String manualPatientId = "";
+    private String organizationId = "";
+    private String priority = "";
+
+    public String getOrderTypeId() {
+        return orderTypeId;
+    }
+
+    public void setOrderTypeId(String orderTypeId) {
+        this.orderTypeId = orderTypeId;
+    }
+
+    public String getOrderNo() {
+        return orderNo;
+    }
+
+    public void setOrderNo(String orderNo) {
+        this.orderNo = orderNo;
+    }
+    
+    public String getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(String patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getCompleteOrderNo() {
+        return completeOrderNo;
+    }
+
+    public void setCompleteOrderNo(String completeOrderNo) {
+        this.completeOrderNo = completeOrderNo;
+    }
+
+    public String getOrderDetailId() {
+        return orderDetailId;
+    }
+
+    public void setOrderDetailId(String orderDetailId) {
+        this.orderDetailId = orderDetailId;
+    }
+
+    public String getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(String locationId) {
+        this.locationId = locationId;
+    }
+
+    public String getLocationDescription() {
+        return locationDescription;
+    }
+
+    public void setLocationDescription(String locationDescription) {
+        this.locationDescription = locationDescription;
+    }
+
+    public String getOrderStatusId() {
+        return orderStatusId;
+    }
+
+    public void setOrderStatusId(String orderStatusId) {
+        this.orderStatusId = orderStatusId;
+    }
+
+    public String getOrderStatusDescription() {
+        return orderStatusDescription;
+    }
+
+    public void setOrderStatusDescription(String orderStatusDescription) {
+        this.orderStatusDescription = orderStatusDescription;
+    }
+
+    public String getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(String orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getInHouseDoctorId() {
+        return inHouseDoctorId;
+    }
+
+    public void setInHouseDoctorId(String inHouseDoctorId) {
+        this.inHouseDoctorId = inHouseDoctorId;
+    }
+
+    public String getInHouseDoctorName() {
+        return inHouseDoctorName;
+    }
+
+    public void setInHouseDoctorName(String inHouseDoctorName) {
+        this.inHouseDoctorName = inHouseDoctorName;
+    }
+
+    public String getReferringDoctorId() {
+        return referringDoctorId;
+    }
+
+    public void setReferringDoctorId(String referringDoctorId) {
+        this.referringDoctorId = referringDoctorId;
+    }
+
+    public String getReferringDoctorName() {
+        return referringDoctorName;
+    }
+
+    public void setReferringDoctorName(String referringDoctorName) {
+        this.referringDoctorName = referringDoctorName;
+    }
+
+    public String getTrnDate() {
+        return trnDate;
+    }
+
+    public void setTrnDate(String trnDate) {
+        this.trnDate = trnDate;
+    }
+
+    public String getOrderBy() {
+        return orderBy;
+    }
+
+    public void setOrderBy(String orderBy) {
+        this.orderBy = orderBy;
+    }
+
+    public String getOrderPerson() {
+        return orderPerson;
+    }
+
+    public void setOrderPerson(String orderPerson) {
+        this.orderPerson = orderPerson;
+    }
+
+    public String getTerminalId() {
+        return terminalId;
+    }
+
+    public void setTerminalId(String terminalId) {
+        this.terminalId = terminalId;
+    }
+
+    public String getMobilityStatusId() {
+        return mobilityStatusId;
+    }
+
+    public void setMobilityStatusId(String mobilityStatusId) {
+        this.mobilityStatusId = mobilityStatusId;
+    }
+
+    public String getOrderLocationId() {
+        return orderLocationId;
+    }
+
+    public void setOrderLocationId(String orderLocationId) {
+        this.orderLocationId = orderLocationId;
+    }
+
+    public String getOrderLocationDescription() {
+        return orderLocationDescription;
+    }
+
+    public void setOrderLocationDescription(String orderLocationDescription) {
+        this.orderLocationDescription = orderLocationDescription;
+    }
+
+    public String getUserLocationId() {
+        return userLocationId;
+    }
+
+    public void setUserLocationId(String userLocationId) {
+        this.userLocationId = userLocationId;
+    }
+
+    public String getUserLocationDescription() {
+        return userLocationDescription;
+    }
+
+    public void setUserLocationDescription(String userLocationDescription) {
+        this.userLocationDescription = userLocationDescription;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getAdmissionNo() {
+        return admissionNo;
+    }
+
+    public void setAdmissionNo(String admissionNo) {
+        this.admissionNo = admissionNo;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getClientDescription() {
+        return clientDescription;
+    }
+
+    public void setClientDescription(String clientDescription) {
+        this.clientDescription = clientDescription;
+    }
+
+    public String getManualPatientId() {
+        return manualPatientId;
+    }
+
+    public void setManualPatientId(String manualPatientId) {
+        this.manualPatientId = manualPatientId;
+    }
+
+    public String getOrganizationId() {
+        return organizationId;
+    }
+
+    public void setOrganizationId(String organizationId) {
+        this.organizationId = organizationId;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+}
