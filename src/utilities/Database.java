5a1a707ed92df359a0c8dd7650b987d4fc73da59
Raheel Ansari <raheelansari7@gmail.com>
2019-12-04 16:54:45 +0500

    CARDIA SURGERY FORM

--- src/utilities/Database.java
+++ src/utilities/Database.java
@@ -13,8 +13,8 @@ public class Database {
         public static final String medicineRequest = DAO.usernameEmr + ".MEDICINE_REQUEST   ";
         public static final String ltuPatientHistory = DAO.usernameEmr + ".LTU_PATIENT_HISTORY   ";
 
-        public static final String cardiacSurgeryMaster = DAO.usernameEmr + ".CARDIAC_SURGERY_MASTER   ";
-        public static final String cardiacHistoryDetail = DAO.usernameEmr + ".CARDIAC_HISTORY_DETAIL   ";
+        public static final String cardiacSurgeryMaster = DAO.usernameEmr + ".CARDIAC_REGISTRY_MASTER   ";
+        public static final String cardiacHistoryDetail = DAO.usernameEmr + ".CARDIAC_REGISTRY_DETAIL   ";
         public static final String cardiacProcedureDetail = DAO.usernameEmr + ".CARDIAC_PROCEDURE_DETAIL   ";
         public static String outsidePatient = DAO.username + "." + "OUTSIDE_PATIENT ";
         public static final String outsidePatientUpdate = DAO.username + ".UPDATE_OUTSIDE_PATIENT ";
